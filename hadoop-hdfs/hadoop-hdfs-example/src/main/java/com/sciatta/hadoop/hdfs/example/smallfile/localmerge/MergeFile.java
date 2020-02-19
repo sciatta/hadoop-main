7c3b6736c6f550ca1f0d7376ad9170d26baca7f8
rain <15058553030@163.com>
2020-02-19 16:21:55 +0800

    重构hdfs目录

--- hadoop-hdfs/hadoop-hdfs-example/src/main/java/com/sciatta/hadoop/hdfs/example/smallfile/localmerge/MergeFile.java
+++ hadoop-hdfs/hadoop-hdfs-example/src/main/java/com/sciatta/hadoop/hdfs/example/smallfile/localmerge/MergeFile.java
@@ -1,32 +1,41 @@
-package com.sciatta.hadoop.hdfs.example.api.impl;
+package com.sciatta.hadoop.hdfs.example.smallfile.localmerge;
 
-import com.sciatta.hadoop.hdfs.example.api.AbstractOperate;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.IOUtils;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
  * Created by yangxiaoyu on 2020/1/12<br>
  * All Rights Reserved(C) 2017 - 2020 SCIATTA<br><p/>
- * MergeFile
+ * MergeFile 通过将客户端多个文件输入流的数据追加到输出流的末尾，从而将若干小文件合并为一个大文件
  */
-public class MergeFile extends AbstractOperate {
-    @Override
-    protected void doWork() throws IOException, URISyntaxException, InterruptedException {
+public class MergeFile {
+    private static final String CFG_DEFAULT_FS_K = "fs.defaultFS";
+    private static final String CFG_DEFAULT_FS_V = "hdfs://192.168.2.100:8020";
+
+    protected static final int BUFFER_SIZE = 4096;
+
+    private Configuration configuration;
+
+    public MergeFile() {
+        configuration = new Configuration();
+        configuration.set(CFG_DEFAULT_FS_K, CFG_DEFAULT_FS_V);
+    }
+
+    private void doWork() throws IOException {
         // hdfs文件系统
-        FileSystem fileSystem = FileSystem.get(getConf());
+        FileSystem fileSystem = FileSystem.get(configuration);
         // local文件系统
         LocalFileSystem localFileSystem = FileSystem.getLocal(new Configuration());
 
         // hdfs输出流
-        FSDataOutputStream fsDataOutputStream = fileSystem.create(new Path("/main/merge"));
+        FSDataOutputStream fsDataOutputStream = fileSystem.create(new Path("/test/hdfs/merge"));
 
         // 读取本地文件
-        FileStatus[] files = localFileSystem.listStatus(new Path("/Users/yangxiaoyu/work/test"));
+        FileStatus[] files = localFileSystem.listStatus(new Path("/Users/yangxiaoyu/work/test/hdfsdatas/merge"));
         for (FileStatus file : files) {
             Path path = file.getPath();
             System.out.println("file: " + path);
--- hadoop-hdfs/hadoop-hdfs-example/src/test/java/com/sciatta/hadoop/hdfs/example/api/AbstractInit.java
+++ hadoop-hdfs/hadoop-hdfs-example/src/test/java/com/sciatta/hadoop/hdfs/example/api/AbstractInit.java
@@ -1,31 +1,28 @@
 package com.sciatta.hadoop.hdfs.example.api;
 
 import org.apache.hadoop.conf.Configuration;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
+import org.junit.Before;
 
 /**
- * Created by yangxiaoyu on 2020/1/12<br>
+ * Created by yangxiaoyu on 2020/2/19<br>
  * All Rights Reserved(C) 2017 - 2020 SCIATTA<br><p/>
- * AbstractOperate
+ * AbstractInit
  */
-public abstract class AbstractOperate {
+public class AbstractInit {
     private static final String CFG_DEFAULT_FS_K = "fs.defaultFS";
     private static final String CFG_DEFAULT_FS_V = "hdfs://192.168.2.100:8020";
 
     private static final String CFG_TRASH_INTERVAL_K = "fs.trash.interval";
     private static final String CFG_TRASH_INTERVAL_V = "10080";  // 分钟，7天
 
-
     protected static final int BUFFER_SIZE = 4096;
 
-    protected Configuration getConf() {
-        Configuration configuration = new Configuration();
+    protected Configuration configuration;
+
+    @Before
+    public void init() {
+        configuration = new Configuration();
         configuration.set(CFG_DEFAULT_FS_K, CFG_DEFAULT_FS_V);
         configuration.set(CFG_TRASH_INTERVAL_K, CFG_TRASH_INTERVAL_V);
-        return configuration;
     }
-
-    protected abstract void doWork() throws IOException, URISyntaxException, InterruptedException;
 }
