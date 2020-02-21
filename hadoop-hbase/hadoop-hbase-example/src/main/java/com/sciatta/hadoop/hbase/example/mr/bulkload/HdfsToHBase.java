a29c00db482ae230e3adb231adc6f95a454b0447
rain <15058553030@163.com>
2020-02-21 17:01:51 +0800

    HBase与MapReduce集成示例：1、HBase或HDFS导入到HBase表；2、使用bulkload方式导入到HBase表

---
+++ hadoop-hbase/hadoop-hbase-example/src/main/java/com/sciatta/hadoop/hbase/example/mr/bulkload/HdfsToHBase.java
@@ -0,0 +1,87 @@
+package com.sciatta.hadoop.hbase.example.mr.bulkload;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.HFileOutputFormat2;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+
+import static com.sciatta.hadoop.hbase.example.mr.bulkload.InitData.*;
+
+/**
+ * Created by yangxiaoyu on 2020/2/21<br>
+ * All Rights Reserved(C) 2017 - 2020 SCIATTA<br><p/>
+ * HdfsToHBase
+ */
+public class HdfsToHBase extends Configured implements Tool {
+
+    static class HdfsReadMapper extends Mapper<LongWritable, Text, ImmutableBytesWritable, Put> {
+        private ImmutableBytesWritable outKey = new ImmutableBytesWritable();
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String[] datas = value.toString().split("\t");
+            String rowKey = datas[0];
+            String name = datas[1];
+            String age = datas[2];
+
+            Put put = new Put(rowKey.getBytes());
+            put.addColumn(DEST_CF.getBytes(), DEST_C_NAME.getBytes(), name.getBytes());
+            put.addColumn(DEST_CF.getBytes(), DEST_C_AGE.getBytes(), age.getBytes());
+
+            outKey.set(rowKey.getBytes());
+
+            context.write(outKey, put);
+        }
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Job job = Job.getInstance(getConf());
+        job.setJarByClass(HdfsToHBase.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        TextInputFormat.addInputPath(job, new Path(HDFS_FILE_PATH));
+
+        job.setMapperClass(HdfsReadMapper.class);
+        job.setMapOutputKeyClass(ImmutableBytesWritable.class);
+        job.setMapOutputValueClass(Put.class);
+
+        configHFileOutputFormat2(job);
+        job.setOutputValueClass(HFileOutputFormat2.class);
+        HFileOutputFormat2.setOutputPath(job, new Path(HDFS_HFILE_PATH));
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    private void configHFileOutputFormat2(Job job) throws IOException {
+        Connection connection = ConnectionFactory.createConnection(getConf());
+        Table table = connection.getTable(TableName.valueOf(DEST_TABLE));
+        HFileOutputFormat2.configureIncrementalLoad(job, table, connection.getRegionLocator(TableName.valueOf(DEST_TABLE)));
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration configuration = HBaseConfiguration.create();
+        configuration.set(ZK_QUORUM_KEY, ZK_QUORUM_VALUE);
+        configuration.set(TMP_DIR_KEY, TMP_DIR_VALUE);
+
+        int test = ToolRunner.run(configuration, new HdfsToHBase(), args);
+        System.exit(test);
+    }
+}
