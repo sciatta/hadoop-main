42a60c709806f9c1aebed63dd5302c017f5aa932
rain <15058553030@163.com>
2020-10-02 08:49:42 +0800

    增加导入流程注释

--- hadoop-hive/hadoop-hive-video/src/main/resources/video_ddl.sql
+++ hadoop-hive/hadoop-hive-video/src/main/resources/video_ddl.sql
@@ -62,10 +62,23 @@ row format delimited
     fields terminated by "\t"
 stored as orc;
 
--- 导入数据到ods
+-- 执行过程 --
+-- 1、原始数据上传
+--      本地user（user.txt） -> /test/hive/video/user（user.txt）
+--      本地videos(0.txt -- 4.txt) -> /test/hive/video/videos(0.txt -- 4.txt)
+
+-- 2、com.sciatta.hadoop.hive.video.etl.ETLRunner
+--      执行ETLRunner，利用MR清洗videos异常数据（ReduceTask=7）
+--      /test/hive/video/videos(0.txt -- 4.txt) -> /test/hive/video/videos_etl（part-r-00000 -- part-r-00006）
+
+-- 3、导入数据到ods（hdfs文件剪切）
+--      /test/hive/video/user（user.txt） -> /user/hive/warehouse/video.db/ods_user（user.txt）
+--      /test/hive/video/videos_etl（part-r-00000 -- part-r-00006） -> /user/hive/warehouse/video.db/ods_video（part-r-00000 -- part-r-00006）
 load data inpath "/test/hive/video/videos_etl" into table ods_video;
 load data inpath "/test/hive/video/user" into table ods_user;
 
--- 导入数据到dw
+-- 4、导入数据到dw
+--      /user/hive/warehouse/video.db/ods_user（user.txt） -> /user/hive/warehouse/video.db/dw_user（000000_0 -- 000023_0） 24桶
+--      /user/hive/warehouse/video.db/ods_video（part-r-00000 -- part-r-00006） -> /user/hive/warehouse/video.db/dw_video（000000_0 -- 000007_0） 8桶
 insert overwrite table dw_video select * from ods_video;
 insert overwrite table dw_user select * from ods_user;
