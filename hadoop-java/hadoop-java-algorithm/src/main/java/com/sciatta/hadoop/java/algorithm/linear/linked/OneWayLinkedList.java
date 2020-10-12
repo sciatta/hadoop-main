7c07b2434b76e5f5e88b8b8a1412f2fbd8a212ca
rain <15058553030@163.com>
2020-10-13 07:10:49 +0800

    单向链表和双向链表实现

---
+++ hadoop-java/hadoop-java-algorithm/src/main/java/com/sciatta/hadoop/java/algorithm/linear/linked/OneWayLinkedList.java
@@ -0,0 +1,115 @@
+package com.sciatta.hadoop.java.algorithm.linear.linked;
+
+import com.sciatta.hadoop.java.algorithm.linear.Linear;
+
+/**
+ * Created by yangxiaoyu on 2020/10/11<br>
+ * All Rights Reserved(C) 2017 - 2020 SCIATTA<br><p/>
+ * 单向链表
+ */
+public class OneWayLinkedList implements Linear<Integer> {
+    private Node head;
+    private int count;
+
+    public OneWayLinkedList() {
+        head = new Node();    // 哨兵，解决边界判断问题
+    }
+
+    /**
+     * 在链表头插入元素
+     *
+     * @param data
+     */
+    @Override
+    public void insert(Integer data) {
+        Node node = new Node(data);
+        Node temp = head.next;
+        head.next = node;
+        node.next = temp;
+        count++;
+    }
+
+    /**
+     * 在指定位置之前插入元素
+     *
+     * @param data
+     * @param i
+     */
+    @Override
+    public void insert(Integer data, int i) {
+        if (i > count) throw new IllegalArgumentException("插入位置不合法 " + i);
+
+        Node pre = head;
+        for (int j = 1; j <= i; j++) {
+            pre = pre.next; // 找到待插入位置的前驱节点
+        }
+
+        Node node = new Node(data);
+        Node temp = pre.next;
+        pre.next = node;
+        node.next = temp;
+        count++;
+    }
+
+    @Override
+    public void delete(Integer data) {
+        Node pre = head;
+        Node cur = head.next;
+
+        while (cur != null) {
+            if (cur.value != null && cur.value.equals(data)) {
+                pre.next = cur.next;
+                cur.next = null;
+                count--;
+                break;
+            } else {
+                pre = cur;
+                cur = cur.next;
+            }
+        }
+    }
+
+    @Override
+    public int find(Integer data) {
+        int index = 0;
+
+        Node cur = head.next;
+
+        while (cur != null) {
+            if (cur.value != null && cur.value.equals(data)) {
+                return index;
+            } else {
+                cur = cur.next;
+                index++;
+            }
+        }
+
+        return -1;
+    }
+
+    static class Node {
+        Integer value;
+        Node next;
+
+        public Node() {
+        }
+
+        public Node(Integer value) {
+            this.value = value;
+        }
+    }
+
+    @Override
+    public Integer[] toArray() {
+        Integer[] ret = new Integer[count];
+        Node cur = head;
+        int index = 0;
+
+        while (cur.next != null) {
+            cur = cur.next;
+            ret[index++] = cur.value;
+        }
+
+        return ret;
+    }
+}
---
+++ hadoop-java/hadoop-java-algorithm/src/test/java/com/sciatta/hadoop/java/algorithm/linear/linked/TwoWayLinkedListTests.java
@@ -0,0 +1,86 @@
+package com.sciatta.hadoop.java.algorithm.linear.linked;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by yangxiaoyu on 2020/10/12<br>
+ * All Rights Reserved(C) 2017 - 2020 SCIATTA<br><p/>
+ * TwoWayLinkedListTests
+ */
+public class TwoWayLinkedListTests {
+    @Test
+    public void testInsertHead() {
+        TwoWayLinkedList list = newLinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        assertArrayEquals(new Integer[]{3, 2, 1}, list.toArray());
+    }
+
+    @Test
+    public void testInsert() {
+        TwoWayLinkedList list = newLinkedList();
+        list.insert(1);
+        list.insert(2);
+
+        list.insert(5, 0);
+        list.insert(3, 2);
+        list.insert(9, 4);
+
+        assertArrayEquals(new Integer[]{5, 2, 3, 1, 9}, list.toArray());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInsertIllegal() {
+        TwoWayLinkedList list = newLinkedList();
+        list.insert(1);
+        list.insert(2);
+
+        list.insert(8, 3);
+    }
+
+    @Test
+    public void testDelete() {
+        TwoWayLinkedList list = newLinkedList();
+        list.insert(1);
+        list.insert(2);
+
+        list.insert(5, 0);
+        list.insert(3, 2);
+        list.insert(9, 4);
+        assertArrayEquals(new Integer[]{5, 2, 3, 1, 9}, list.toArray());
+
+        list.delete(5);
+        assertArrayEquals(new Integer[]{2, 3, 1, 9}, list.toArray());
+
+        list.delete(3);
+        assertArrayEquals(new Integer[]{2, 1, 9}, list.toArray());
+
+        list.delete(9);
+        assertArrayEquals(new Integer[]{2, 1}, list.toArray());
+
+        list.delete(10);
+        assertArrayEquals(new Integer[]{2, 1}, list.toArray());
+    }
+
+    @Test
+    public void testFind() {
+        TwoWayLinkedList list = newLinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+
+        assertEquals(0, list.find(3));
+        assertEquals(1, list.find(2));
+        assertEquals(2, list.find(1));
+
+        assertEquals(-1, list.find(8));
+    }
+
+    private TwoWayLinkedList newLinkedList() {
+        return new TwoWayLinkedList();
+    }
+}
