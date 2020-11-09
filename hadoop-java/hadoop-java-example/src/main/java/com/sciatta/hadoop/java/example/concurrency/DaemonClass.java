5376347f25bef4b1d577bca06fd3b01fc962b267
yangxiaoyu <15058553030@163.com>
2020-11-09 09:14:02 +0800

    DaemonClass修改例子

--- hadoop-java/hadoop-java-example/src/main/java/com/sciatta/hadoop/java/example/concurrency/DaemonClass.java
+++ hadoop-java/hadoop-java-example/src/main/java/com/sciatta/hadoop/java/example/concurrency/DaemonClass.java
@@ -8,22 +8,21 @@ package com.sciatta.hadoop.java.example.concurrency;
 public class DaemonClass {
     public static void main(String[] args) {
         class Daemon implements Runnable {
-
             @Override
             public void run() {
-                for (; ; ) {
-                    try {
-                        Thread.sleep(200);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
+
+                // 如果是用户线程，则此处一定执行
+                System.out.println(Thread.currentThread().getName());
             }
         }
 
-        Thread t = new Thread(new Daemon(), "t");   // 默认是用户线程，不会随着main线程退出而退出
+        Thread t = new Thread(new Daemon(), "test-thread-1");   // 默认是用户线程，不会随着main线程退出而退出
         t.setDaemon(true);  // 后台线程，随着main线程退出而退出
-        System.out.println("Daemon=" + t.isDaemon());
         t.start();
     }
 }
