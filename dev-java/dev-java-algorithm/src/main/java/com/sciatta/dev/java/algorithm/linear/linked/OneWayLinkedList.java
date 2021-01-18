45d4560f09ff9a8aef18d4de86e74f89e3d0bf09
yxyuhz <yxyuhz@163.com>
2021-01-18 08:42:50 +0800

    refactor: refactor code that adding readable

--- dev-java/dev-java-algorithm/src/main/java/com/sciatta/dev/java/algorithm/linear/linked/OneWayLinkedList.java
+++ dev-java/dev-java-algorithm/src/main/java/com/sciatta/dev/java/algorithm/linear/linked/OneWayLinkedList.java
@@ -10,11 +10,11 @@ import com.sciatta.dev.java.algorithm.linear.Linear;
 public class OneWayLinkedList implements Linear<Integer> {
     private Node head;
     private int count;
-
+    
     public OneWayLinkedList() {
         head = new Node();    // 哨兵，解决边界判断问题
     }
-
+    
     /**
      * 在链表头插入元素
      *
@@ -23,12 +23,13 @@ public class OneWayLinkedList implements Linear<Integer> {
     @Override
     public void insert(Integer data) {
         Node node = new Node(data);
-        Node temp = head.next;
+        
+        node.next = head.next;
         head.next = node;
-        node.next = temp;
+        
         count++;
     }
-
+    
     /**
      * 在指定位置之前插入元素
      *
@@ -38,24 +39,29 @@ public class OneWayLinkedList implements Linear<Integer> {
     @Override
     public void insert(Integer data, int i) {
         if (i > count) throw new IllegalArgumentException("插入位置不合法 " + i);
-
+        
+        if (i == 0) {
+            insert(data);   // 插入到头节点之后
+            return;
+        }
+        
         Node pre = head;
-        for (int j = 1; j <= i; j++) {
+        for (int j = 0; j < i; j++) {
             pre = pre.next; // 找到待插入位置的前驱节点
         }
-
+        
         Node node = new Node(data);
-        Node temp = pre.next;
+        node.next = pre.next;
         pre.next = node;
-        node.next = temp;
+        
         count++;
     }
-
+    
     @Override
     public void delete(Integer data) {
         Node pre = head;
         Node cur = head.next;
-
+        
         while (cur != null) {
             if (cur.value != null && cur.value.equals(data)) {
                 pre.next = cur.next;
@@ -68,13 +74,13 @@ public class OneWayLinkedList implements Linear<Integer> {
             }
         }
     }
-
+    
     @Override
     public int find(Integer data) {
         int index = 0;
-
+        
         Node cur = head.next;
-
+        
         while (cur != null) {
             if (cur.value != null && cur.value.equals(data)) {
                 return index;
@@ -83,33 +89,33 @@ public class OneWayLinkedList implements Linear<Integer> {
                 index++;
             }
         }
-
+        
         return -1;
     }
-
+    
     static class Node {
         Integer value;
         Node next;
-
+        
         public Node() {
         }
-
+        
         public Node(Integer value) {
             this.value = value;
         }
     }
-
+    
     @Override
     public Integer[] toArray() {
         Integer[] ret = new Integer[count];
         Node cur = head;
         int index = 0;
-
+        
         while (cur.next != null) {
             cur = cur.next;
             ret[index++] = cur.value;
         }
-
+        
         return ret;
     }
 }
