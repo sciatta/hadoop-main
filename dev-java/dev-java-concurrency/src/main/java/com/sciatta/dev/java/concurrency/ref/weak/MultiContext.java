83381ff4b5276f2f9b35f4dd9e64e2bcd67c47e5
yxyyyt <yxyyyt@126.com>
2021-04-05 21:53:23 +0800

    refactor: ThreadLocal OOM example

--- dev-java/dev-java-concurrency/src/main/java/com/sciatta/dev/java/concurrency/ref/weak/MultiContext.java
+++ dev-java/dev-java-concurrency/src/main/java/com/sciatta/dev/java/concurrency/ref/weak/MultiContext.java
@@ -3,16 +3,21 @@ package com.sciatta.dev.java.concurrency.ref.weak;
 /**
  * Created by yangxiaoyu on 2021/4/4<br>
  * All Rights Reserved(C) 2017 - 2021 SCIATTA<br><p/>
- * MultiContext
+ * MultiContext<p/>
+ * -Xms20m -Xmx20m -XX:+PrintGCDetails -XX:+PrintGCDateStamps
  */
 public class MultiContext {
-    private static ThreadLocal<String> c1 = new ThreadLocal<>();
-    private static ThreadLocal<String> c2 = new ThreadLocal<>();
+    private static final int _1m = 1024 * 1024;
     
     public static void main(String[] args) {
-        c1.set("1");
-        // map->key thread local
-        //    ->value
-        c2.set("A");
+        ThreadLocal<byte[]> c1 = new ThreadLocal<>();
+        ThreadLocal<byte[]> c2 = new ThreadLocal<>();
+        
+        c1.set(new byte[_1m * 10]);
+        c1.remove();    // 需要调用remove，否则挂在线程下的本地变量无法GC回收，导致OOM
+        c1 = null;
+        System.gc();
+        
+        c2.set(new byte[_1m * 6]);
     }
 }
